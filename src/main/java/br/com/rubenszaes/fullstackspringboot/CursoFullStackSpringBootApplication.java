69c4439c4f13371360eff479994f6e56eb9c4352
Rubens Zaes <rubenszaes@gmail.com>
2022-09-27 22:38:29 -0300

    first commit

---
+++ src/main/java/br/com/rubenszaes/fullstackspringboot/CursoFullStackSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.rubenszaes.fullstackspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoFullStackSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoFullStackSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rubenszaes/fullstackspringboot/CursoFullStackSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rubenszaes.fullstackspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoFullStackSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
